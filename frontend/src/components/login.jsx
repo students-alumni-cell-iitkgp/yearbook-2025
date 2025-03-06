9b956fb4e0dff13a5bd4c18d6cfb52cddc189013
nirmal-2512 <nirmalpatidar2533@gmail.com>
2025-03-06 23:23:39 +0530

    Login Page Update

--- frontend/src/components/login.jsx
+++ frontend/src/components/login.jsx
@@ -5,6 +5,7 @@ import './login.css'; // Import your CSS file
 function LoginPage() {
   return (
   <div className="main-contanier">
+    <div className="sac-name">STUDENTS' ALUMNI CELL</div>
   <div class="login-container">
   <div class="logo-container">
     <img src="../../src/img/kgplogo_white.png" alt="Institution Logo" />
@@ -17,8 +18,8 @@ function LoginPage() {
     <div class="form-group centre-align">
     <input type="text" placeholder="21DD10090" className="login-roll"/>
     <input type="text" placeholder="22122002" className="login-dob"/>
-      <button class="btn-login" type="submit">LOGIN</button>
-      <small class="text-muted d-block text-right mt-3">
+      <button class="btn-login btn" type="submit">LOGIN</button>
+      <small class="btn-test btn">
         <a href="">Write Testinomials</a>
       </small>
     </div>
